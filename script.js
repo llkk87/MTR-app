a85910773af0e184b9c0c40c5ab84ce8e10d38ec
Paul, Chan Ka Yin <yt061078@gmail.com>
2024-03-02 18:38:49 +0800

    upload files

---
+++ script.js
@@ -0,0 +1,336 @@
+line = {
+  KTL: {
+    text: "觀塘線",
+    color: "#7ed957",
+    sta: [
+      { code: "WHA", name: "Whampoa" },
+      { code: "HOM", name: "Ho Man Tin" },
+      { code: "YMT", name: "Yau Ma Tei" },
+      { code: "MOK", name: "Mong Kok" },
+      { code: "PRE", name: "Prince Edward" },
+      { code: "SKM", name: "Shek Kip Mei" },
+      { code: "KOT", name: "Kowloon Tong" },
+      { code: "LOF", name: "Lok Fu" },
+      { code: "WTS", name: "Wong Tai Sin" },
+      { code: "DIH", name: "Diamond Hill" },
+      { code: "CHH", name: "Choi Hung" },
+      { code: "KOB", name: "Kowloon Bay" },
+      { code: "NTK", name: "Ngau Tau Kok" },
+      { code: "KWT", name: "Kwun Tong" },
+      { code: "LAT", name: "Lam Tin" },
+      { code: "YAT", name: "Yau Tong" },
+      { code: "TIK", name: "Tiu Keng Leng" },
+    ],
+  },
+  ISL: {
+    text: "港島線",
+    color: "#004aad",
+    sta: [
+      { code: "KET", name: "Kennedy Town" },
+      { code: "HKU", name: "HKU" },
+      { code: "SYP", name: "Sai Ying Pun" },
+      { code: "SHW", name: "Sheung Wan" },
+      { code: "CEN", name: "Central" },
+      { code: "ADM", name: "Admiralty" },
+      { code: "WAC", name: "Wan Chai" },
+      { code: "CAB", name: "Causeway Bay" },
+      { code: "TIH", name: "Tin Hau" },
+      { code: "FOH", name: "Fortress Hill" },
+      { code: "NOP", name: "North Point" },
+      { code: "QUB", name: "Quarry Bay" },
+      { code: "TAK", name: "Tai Koo" },
+      { code: "SWH", name: "Sai Wan Ho" },
+      { code: "SKW", name: "Shau Kei Wan" },
+      { code: "HFC", name: "Heng Fa Chuen" },
+      { code: "CHW", name: "Chai Wan" },
+    ],
+  },
+  TWL: {
+    text: "荃灣線",
+    color: "#ff3131",
+    sta: [
+      { code: "CEN", name: "Central" },
+      { code: "ADM", name: "Admiralty" },
+      { code: "TST", name: "Tsim Sha Tsui" },
+      { code: "JOR", name: "Jordan" },
+      { code: "YMT", name: "Yau Ma Tei" },
+      { code: "MOK", name: "Mong Kok" },
+      { code: "CEN", name: "Central" },
+      { code: "ADM", name: "Admiralty" },
+      { code: "TST", name: "Tsim Sha Tsui" },
+      { code: "JOR", name: "Jordan" },
+      { code: "YMT", name: "Yau Ma Tei" },
+      { code: "MOK", name: "Mong Kok" },
+    ],
+  },
+  SIL: {
+    text: "南港島線",
+    color: "#cbcd09",
+    sta: [
+      { code: "ADM", name: "Admiralty" },
+      { code: "OCP", name: "Ocean Park" },
+      { code: "WCH", name: "Wong Chuk Hang" },
+      { code: "LET", name: "Lei Tung" },
+      { code: "SOH", name: "South Horizons" },
+    ],
+  },
+  TCL: {
+    text: "東涌線",
+    color: "#f6943d",
+    sta: [
+      { code: "HOK", name: "Hong Kong" },
+      { code: "KOW", name: "Kowloon" },
+      { code: "OLY", name: "Olympic" },
+      { code: "NAC", name: "Nam Cheong" },
+      { code: "LAK", name: "Lai King" },
+      { code: "TSY", name: "Tsing Yi" },
+      { code: "SUN", name: "Sunny Bay" },
+      { code: "TUC", name: "Tung Chung" },
+    ],
+  },
+  EAL: {
+    text: "東鐵線",
+    color: "#5ce1e6",
+    sta: [
+      { code: "ADM", name: "Admiralty" },
+      { code: "EXC", name: "Exhibition Centre" },
+      { code: "HUH", name: "Hung Hom" },
+      { code: "MKK", name: "Mong Kok East" },
+      { code: "KOT", name: "Kowloon Tong" },
+      { code: "TAW", name: "Tai Wai" },
+      { code: "SHT", name: "Sha Tin" },
+      { code: "FOT", name: "Fo Tan" },
+      { code: "RAC", name: "Racecourse" },
+      { code: "UNI", name: "University" },
+      { code: "TAP", name: "Tai Po Market" },
+      { code: "TWO", name: "Tai Wo" },
+      { code: "FAN", name: "Fanling" },
+      { code: "SHS", name: "Sheung Shui" },
+      { code: "LOW", name: "Lo Wu" },
+      { code: "LMC", name: "Lok Ma Chau" },
+    ],
+  },
+  TML: {
+    text: "屯馬線",
+    color: "#8d6019",
+    sta: [
+      { code: "WKS", name: "Wu Kai Sha" },
+      { code: "MOS", name: "Ma On Shan" },
+      { code: "HEO", name: "Heng On" },
+      { code: "TSH", name: "Tai Shui Hang" },
+      { code: "SHM", name: "Shek Mun" },
+      { code: "CIO", name: "City One" },
+      { code: "STW", name: "Sha Tin Wai" },
+      { code: "CKT", name: "Che Kung Temple" },
+      { code: "TAW", name: "Tai Wai" },
+      { code: "HIK", name: "Hin Keng" },
+      { code: "DIH", name: "Diamond Hill" },
+      { code: "KAT", name: "Kai Tak" },
+      { code: "SUW", name: "Sung Wong Toi" },
+      { code: "TKW", name: "To Kwa Wan" },
+      { code: "HOM", name: "Ho Man Tin" },
+      { code: "HUH", name: "Hung Hom" },
+      { code: "ETS", name: "East Tsim Sha Tsui" },
+      { code: "AUS", name: "Austin" },
+      { code: "NAC", name: "Nam Cheong" },
+      { code: "MEF", name: "Mei Foo" },
+      { code: "TWW", name: "Tsuen Wan West" },
+      { code: "KSR", name: "Kam Sheung Road" },
+      { code: "YUL", name: "Yuen Long" },
+      { code: "LOP", name: "Long Ping" },
+      { code: "TIS", name: "Tin Shui Wai" },
+      { code: "SIH", name: "Siu Hong" },
+      { code: "TUM", name: "Tuen Mun" },
+    ],
+  },
+  AEL: {
+    text: "機場快線",
+    color: "#3d9ea0",
+    sta: [
+      { code: "HOK", name: "Hong Kong" },
+      { code: "KOW", name: "Kowloon" },
+      { code: "TSY", name: "Tsing Yi" },
+      { code: "AIR", name: "Airport" },
+      { code: "AWE", name: "AsiaWorld Expo" },
+    ],
+  },
+  TKL: {
+    text: "將軍澳線",
+    color: "#8d45ab",
+    sta: [
+      { code: "NOP", name: "North Point" },
+      { code: "QUB", name: "Quarry Bay" },
+      { code: "YAT", name: "Yau Tong" },
+      { code: "TIK", name: "Tiu Keng Leng" },
+      { code: "TKO", name: "Tseung Kwan O" },
+      { code: "LHP", name: "LOHAS Park" },
+      { code: "HAH", name: "Hang Hau" },
+      { code: "POA", name: "Po Lam" },
+    ],
+  },
+};
+
+let btnAel = document.querySelector(".btn-ael");
+let btnTcl = document.querySelector(".btn-tcl");
+let btnTml = document.querySelector(".btn-tml");
+let btnKtl = document.querySelector(".btn-ktl");
+let btnTwl = document.querySelector(".btn-twl");
+let btnIsl = document.querySelector(".btn-isl");
+let btnTkl = document.querySelector(".btn-tkl");
+let btnSil = document.querySelector(".btn-sil");
+let btnEal = document.querySelector(".btn-eal");
+let lastUpdate = document.querySelector(".last-update");
+let container = document.querySelector(".container");
+let button = document.querySelectorAll("button");
+let url, response, data, code;
+
+function buttonActive(className) {
+  button.forEach((button) => button.classList.remove("lightblue"));
+  document.querySelector(`.${className}`).classList.add("lightblue");
+}
+
+function clearContainer() {
+  let container = document.querySelector(".container");
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+function createStationCards(direction, nameData, nextTrainData, platformData) {
+  let stationCard = document.createElement("div");
+  stationCard.setAttribute("class", "station-card");
+  container.appendChild(stationCard);
+
+  let stationName = document.createElement("div");
+  stationName.setAttribute("class", "station-info station-name");
+  stationName.textContent = `${nameData} (${direction})`;
+  stationCard.appendChild(stationName);
+
+  let nextTrain = document.createElement("div");
+  nextTrain.setAttribute("class", "station-info station-next-train");
+  nextTrain.textContent = `Next Train: ${nextTrainData.slice(11)}`;
+  stationCard.appendChild(nextTrain);
+
+  let platform = document.createElement("div");
+  platform.setAttribute("class", "station-info station-platform");
+  platform.textContent = `Platform: ${platformData}`;
+  stationCard.appendChild(platform);
+}
+
+function myFn(lineName) {
+  async function getData() {
+    try {
+      for (let i = 0; i < line[lineName].sta.length - 1; i++) {
+        code = line[lineName].sta[i].code;
+        url = `https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=${lineName}&sta=${code}`;
+
+        response = await fetch(url);
+        data = await response.json();
+
+        lastUpdate.textContent = `Last Update: ${data.sys_time}`;
+
+        let nameData = line[lineName].sta[i].name;
+        let nextTrainData, platformData;
+
+        if (
+          data.data[`${lineName}-${line[lineName].sta[i].code}`] &&
+          data.data[`${lineName}-${line[lineName].sta[i].code}`]["UP"] &&
+          data.data[`${lineName}-${line[lineName].sta[i].code}`]["UP"][0] &&
+          data.data[`${lineName}-${line[lineName].sta[i].code}`]["UP"][0].time
+        ) {
+          nextTrainData = data.data[`${lineName}-${line[lineName].sta[i].code}`]["UP"][0].time;
+        } else {
+          continue;
+        }
+
+        platformData = data.data[`${lineName}-${line[lineName].sta[i].code}`]["UP"][0].plat;
+
+        createStationCards("UP", nameData, nextTrainData, platformData);
+      }
+
+      for (let j = line[lineName].sta.length - 1; j >= 1; j--) {
+        code = line[lineName].sta[j].code;
+        url = `https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=${lineName}&sta=${code}`;
+
+        response = await fetch(url);
+        data = await response.json();
+
+        let nameData = line[lineName].sta[j].name;
+        let nextTrainData, platformData;
+
+        if (
+          data.data[`${lineName}-${line[lineName].sta[j].code}`] &&
+          data.data[`${lineName}-${line[lineName].sta[j].code}`]["DOWN"] &&
+          data.data[`${lineName}-${line[lineName].sta[j].code}`]["DOWN"][0] &&
+          data.data[`${lineName}-${line[lineName].sta[j].code}`]["DOWN"][0].time
+        ) {
+          nextTrainData = data.data[`${lineName}-${line[lineName].sta[j].code}`]["DOWN"][0].time;
+        } else {
+          continue;
+        }
+
+        platformData = data.data[`${lineName}-${line[lineName].sta[j].code}`]["DOWN"][0].plat;
+
+        createStationCards("DOWN", nameData, nextTrainData, platformData);
+      }
+    } catch (error) {
+      console.error("error:", error);
+    }
+  }
+  getData();
+}
+
+btnAel.addEventListener("click", async function () {
+  buttonActive("btn-ael");
+  clearContainer();
+  myFn("AEL");
+});
+
+btnTcl.addEventListener("click", function () {
+  buttonActive("btn-tcl");
+  clearContainer();
+  myFn(`TCL`);
+});
+
+btnTml.addEventListener("click", function () {
+  buttonActive("btn-tml");
+  clearContainer();
+  myFn("TML");
+});
+
+btnKtl.addEventListener("click", function () {
+  buttonActive("btn-ktl");
+  clearContainer();
+  myFn("KTL");
+});
+
+btnTwl.addEventListener("click", function () {
+  buttonActive("btn-twl");
+  clearContainer();
+  myFn("TWL");
+});
+
+btnIsl.addEventListener("click", function () {
+  buttonActive("btn-isl");
+  clearContainer();
+  myFn("ISL");
+});
+
+btnTkl.addEventListener("click", function () {
+  buttonActive("btn-tkl");
+  clearContainer();
+  myFn("TKL");
+});
+
+btnSil.addEventListener("click", function () {
+  buttonActive("btn-sil");
+  clearContainer();
+  myFn("SIL");
+});
+
+btnEal.addEventListener("click", function () {
+  btnEal.style.color = "black";
+  buttonActive("btn-eal");
+  clearContainer();
+  myFn("EAL");
+});
